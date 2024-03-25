9be9876947fa24dab2fad3b858c967164a13487f
NguyenHoaiNgoc <164768732+NguyenHoaiNgoc@users.noreply.github.com>
2024-03-25 16:31:21 +0700

    commit app_exam,iris,file pkl

---
+++ app_exam.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_iris
+import streamlit
+import pickle
+
+iris_data = load_iris()
+model = pickle.load(open('iris_lrg_model.pkl', 'rb'))
+
+#tạo thanh ngang 
+streamlit.sidebar.title('Choose the sepal/petal length/width')
+sepal_length = streamlit.sidebar.slider('Sepal Length', 4.0, 8.0, 5.0)
+sepal_width = streamlit.sidebar.slider('Sepal Width', 2.0, 4.5, 3.0)
+petal_length = streamlit.sidebar.slider('Petal Length', 1.0, 7.0, 4.0)
+petal_width = streamlit.sidebar.slider('Petal Width', 0.1, 2.5, 1.0)
+
+#dự đoán kết quả
+prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+streamlit.write('RESULT:')
+streamlit.write(iris_data.target_names[prediction[0]])
