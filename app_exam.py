efcffbd88db7afee4455e0faad32ecc0dd5e0144
NguyenHoaiNgoc <164768732+NguyenHoaiNgoc@users.noreply.github.com>
2024-03-25 17:09:19 +0700

    change app_exam

--- app_exam.py
+++ app_exam.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 import streamlit
 import pickle
+import numpy
 
 iris_data = load_iris()
 model = pickle.load(open('iris_lrg_model.pkl', 'rb'))
@@ -11,6 +12,12 @@ sepal_length = streamlit.sidebar.slider('Sepal Length', 4.0, 8.0, 5.0)
 sepal_width = streamlit.sidebar.slider('Sepal Width', 2.0, 4.5, 3.0)
 petal_length = streamlit.sidebar.slider('Petal Length', 1.0, 7.0, 4.0)
 petal_width = streamlit.sidebar.slider('Petal Width', 0.1, 2.5, 1.0)
+
+sepal_length = np.nan_to_num(sepal_length)
+sepal_width = np.nan_to_num(sepal_width)
+petal_length = np.nan_to_num(petal_length)
+petal_width = np.nan_to_num(petal_width)
+
 
 #dự đoán kết quả
 prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
